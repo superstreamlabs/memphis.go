8d9ccd660a73e499e9490e6a75de54385badcfda
Or Grinberg <or@memphis.dev>
2022-07-10 08:12:45 +0300

    Update examples

--- examples/consumer.go
+++ examples/consumer.go
@@ -3,38 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/memphisdev/memphis.go"
 )
 
 func main() {
-	conn, err := memphis.Connect("localhost", "root", "memphis")
+	conn, err := memphis.Connect("<memphis-host>", "<application type username>", "<broker-token>")
 	if err != nil {
 		os.Exit(1)
 	}
 	defer conn.Close()
 
-	consumer, err := conn.CreateConsumer("station_1", "consumer_name")
+	consumer, err := conn.CreateConsumer("<station-name>", "<consumer-name>")
 
 	if err != nil {
 		fmt.Printf("Consumer creation failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	msgs, err := consumer.Fetch()
-	if err != nil {
-		fmt.Printf("Fetch failed: %v\n", err)
-		os.Exit(1)
+	handler := func(msgs []*memphis.Msg, err error) {
+		if err != nil {
+			fmt.Printf("Fetch failed: %v\n", err)
+			return
+		}
+
+		for _, msg := range msgs {
+			fmt.Println(string(msg.Data()))
+			msg.Ack()
+		}
 	}
 
-	for _, msg := range msgs {
-		fmt.Println(string(msg.Data()))
-		msg.Ack()
-	}
+	consumer.Consume(15*time.Second, handler)
 
-	err = consumer.Destroy()
-	if err != nil {
-		fmt.Printf("Consumer removal failed: %v\n", err)
-		os.Exit(1)
-	}
+	time.Sleep(30 * time.Second)
 }
--- examples/producer.go
+++ examples/producer.go
@@ -8,13 +8,13 @@ import (
 )
 
 func main() {
-	c, err := memphis.Connect("localhost", "root", "memphis")
+	c, err := memphis.Connect("<memphis-host>", "<application type username>", "<broker-token>")
 	if err != nil {
 		os.Exit(1)
 	}
 	defer c.Close()
 
-	p, err := c.CreateProducer("station_1", "producer_name_a")
+	p, err := c.CreateProducer("<station-name>", "<producer-name>")
 	err = p.Produce([]byte("You have a message!"))
 
 	if err != nil {
