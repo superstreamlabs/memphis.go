028225feff286ed502400d3e153916ee9ed8a1b5
idanasulinStrech <idan@memphis.dev>
2023-11-09 13:27:39 +0200

    fixing comments

--- functions.go
+++ functions.go
@@ -2,6 +2,8 @@ package memphis
 
 import (
 	"context"
+	"encoding/base64"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -10,10 +12,10 @@ type MemphisMsg struct {
 	Payload string            `json:"payload"`
 }
 
-type MemphisMsgWithError struct{
+type MemphisMsgWithError struct {
 	Headers map[string]string `json:"headers"`
 	Payload string            `json:"payload"`
-	Error string			  `json:"error"`
+	Error   string            `json:"error"`
 }
 
 type MemphisEvent struct {
@@ -21,37 +23,54 @@ type MemphisEvent struct {
 }
 
 type MemphisOutput struct {
-	Messages []MemphisMsg `json:"messages"`
-	FailedMessages []MemphisMsgWithError `json:"failedMessages"`
+	Messages       []MemphisMsg          `json:"messages"`
+	FailedMessages []MemphisMsgWithError `json:"failed_messages"`
 }
 
-type UserFunction func(string) (string, error)
+// EventHandlerFunction gets the message payload as []byte and message headers as map[string]string and should return the modified payload and headers.
+// error should be returned if the message should be considered failed and go into the dead-letter station.
+// if all returned values are nil the message will be filtered out of the station.
+type EventHandlerFunction func([]byte, map[string]string) ([]byte, map[string]string, error)
 
-func CreateFunction(userFunction UserFunction){
+// CreateFunction creates a Memphis function
+// eventHandlerFunction gets the message payload as []byte and message headers as map[string]string and should return the modified payload and headers.
+// error should be returned if the message should be considered failed and go into the dead-letter station.
+// if all returned values are nil the message will be filtered out of the station.
+func CreateFunction(eventHandler EventHandlerFunction) {
 	LambdaHandler := func(ctx context.Context, event *MemphisEvent) (*MemphisOutput, error) {
 		var processedEvent MemphisOutput
-		for _, msg := range event.Messages {	
-			modifiedPayload, err := userFunction(msg.Payload)
-	
-			if err != nil{
+		for _, msg := range event.Messages {
+			payload, err := base64.StdEncoding.DecodeString(msg.Payload)
+			if err != nil {
 				processedEvent.FailedMessages = append(processedEvent.FailedMessages, MemphisMsgWithError{
 					Headers: msg.Headers,
 					Payload: msg.Payload,
-					Error: err.Error(),
+					Error:   "couldn't decode message: " + err.Error(),
 				})
-	
 				continue
 			}
-	
-			processedEvent.Messages = append(processedEvent.Messages, MemphisMsg{
-				Headers: msg.Headers,
-				Payload: modifiedPayload,
-			})
+
+			modifiedPayload, modifiedHeaders, err := eventHandler(payload, msg.Headers)
+			if err != nil {
+				processedEvent.FailedMessages = append(processedEvent.FailedMessages, MemphisMsgWithError{
+					Headers: msg.Headers,
+					Payload: msg.Payload,
+					Error:   err.Error(),
+				})
+				continue
+			}
+
+			if modifiedPayload != nil || modifiedHeaders != nil {
+				modifiedPayloadStr := base64.StdEncoding.EncodeToString(modifiedPayload)
+				processedEvent.Messages = append(processedEvent.Messages, MemphisMsg{
+					Headers: modifiedHeaders,
+					Payload: modifiedPayloadStr,
+				})
+			}
 		}
-	
+
 		return &processedEvent, nil
 	}
 
 	lambda.Start(LambdaHandler)
 }
-
