311925ba3eb66c2cc41e29b13f605c1e044579ef
daniel-davidd <daniel@dnyls-MacBook-Pro.local>
2024-01-01 16:20:32 +0200

    changed the dls subject

--- utils.go
+++ utils.go
@@ -10,5 +10,9 @@ func memphisError(err error) error {
 		return nil
 	}
 	message := strings.Replace(err.Error(), "nats", "memphis", -1)
+
+	if strings.Contains(message, "Permissions Violation") {
+		return errors.New("memphis: Permissions Violation")
+	}
 	return errors.New(message)
 }
